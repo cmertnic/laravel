714e2363b3bea6e694bd0d776cdfbd22a955e29d
cmertnic <lskutin@yandex.ru>
2024-10-17 18:01:29 +0500

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources//*.blade.php",
-    "./resources//*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-    colors:{
-      "red":"#F85149"
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [],
-}
+
+    plugins: [forms,  require('flowbite/plugin')],
+    
+};
